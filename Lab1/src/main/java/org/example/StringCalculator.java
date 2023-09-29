030878f989dacdfe49e72cde9163ea9e8e6b5738
Kateryna Tsaruk <kat31774782@gmail.com>
2023-09-29 22:28:12 +0300

    Написання тестів до вимог першого кроку, створення класу та методу

--- Lab1/src/main/java/org/example/StringCalculator.java
+++ Lab1/src/main/java/org/example/StringCalculator.java
@@ -1,4 +1,5 @@
 package org.example;
 
 public class StringCalculator {
+    public int add(String numbers) {return 0;}
 }
--- Lab1/src/test/java/org/example/StringCalculatorTest.java
+++ Lab1/src/test/java/org/example/StringCalculatorTest.java
@@ -1,4 +1,32 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class StringCalculatorTest {
+    StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void emptyStringCase(){
+        int result = calculator.add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    void onlyDelimetrCase(){
+        int result = calculator.add(",");
+        assertEquals(0, result);
+    }
+
+    @Test
+    void oneNumberCase(){
+        int result = calculator.add("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    void classicCase(){
+        int result = calculator.add("1,2");
+        assertEquals(3, result);
+    }
 }
