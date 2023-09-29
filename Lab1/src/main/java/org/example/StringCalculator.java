fb2b12e1614fe3377b61d5cb39903c4b0597a965
Kateryna Tsaruk <kat31774782@gmail.com>
2023-09-29 22:29:53 +0300

    Виконання першого кроку лабораторної

--- Lab1/src/main/java/org/example/StringCalculator.java
+++ Lab1/src/main/java/org/example/StringCalculator.java
@@ -1,5 +1,17 @@
 package org.example;
 
 public class StringCalculator {
-    public int add(String numbers) {return 0;}
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        else {
+            String[] numberArray = numbers.split(",");
+            int sum = 0;
+            for (String num : numberArray) {
+                sum += Integer.parseInt(num);
+            }
+            return sum;
+        }
+    }
 }
