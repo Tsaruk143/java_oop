037c087b6c045b8c38c6e7f6dd94ca237f5380d1
Kateryna Tsaruk <kat31774782@gmail.com>
2023-10-05 22:45:27 +0300

    Виконання кроку 5

--- Lab1/src/main/java/org/example/Main.java
+++ Lab1/src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
-
 public class Main {
-
+    public static void main(String[] args) {
+        try {
+            StringCalculator calculator = new StringCalculator();
+            System.out.println(calculator.add("1,-1,2,-2,3"));
+        }
+        catch (Exception e)
+        {System.out.println(e);}
+    }
 }
--- Lab1/src/main/java/org/example/StringCalculator.java
+++ Lab1/src/main/java/org/example/StringCalculator.java
@@ -1,35 +1,51 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
 public class StringCalculator {
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        else {
-            String delimetr = ",";
-
-            if (numbers.startsWith("//")) {
-                int delimetrEnd = numbers.indexOf("\n");
-                delimetr = numbers.substring(2, delimetrEnd);
-                numbers = numbers.substring(delimetrEnd + 1);
-            }
-
-            numbers = numbers.replaceAll(delimetr, ",");
-            numbers = numbers.replaceAll("\n", ",");
-
-            if (numbers.contains(",,")) {
-                System.out.println("Ви не можете вводити деліметри один за одним.");
+    public int add(String numbers) throws IllegalArgumentException {
+        try {
+            if (numbers.isEmpty()) {
                 return 0;
+            } else {
+                String delimiter = ",";
+
+                if (numbers.startsWith("//")) {
+                    int delimiterEnd = numbers.indexOf("\n");
+                    delimiter = numbers.substring(2, delimiterEnd);
+                    numbers = numbers.substring(delimiterEnd + 1);
+                }
+
+                numbers = numbers.replaceAll(delimiter, ",");
+                numbers = numbers.replaceAll("\n", ",");
+
+                if (numbers.contains(",,")) {
+                    System.out.println("Помилка: дві коми підряд недопустимі.");
+                    return 0;
+                }
+
+                String[] numberArray = numbers.split(",");
+
+                int sum = 0;
+                List<Integer> negativeNumbers = new ArrayList<>();
+
+                for (String num : numberArray) {
+                    int value = Integer.parseInt(num);
+                    if (value < 0) {
+                        negativeNumbers.add(value);
+                    }
+                    sum += value;
+                }
+
+                if (!negativeNumbers.isEmpty()) {
+                    System.out.println("Помилка: недозволені від'ємні числа: " + negativeNumbers);
+                    throw new IllegalArgumentException();
+                }
+
+                return sum;
             }
-
-            String[] numberArray = numbers.split(",");
-
-
-            int sum = 0;
-            for (String num : numberArray) {
-                sum += Integer.parseInt(num);
-            }
-            return sum;
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException(e);
         }
     }
 
